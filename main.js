58efbecb28880df06981ebf9978a68aabdf0dff4
Lingkon <lingkon909@gmail.com>
2023-04-20 01:21:59 +0600

    updated accordion

--- main.js
+++ main.js
@@ -36,3 +36,22 @@ $(document).ready(function () {
 		autoplaySpeed: 2000,
 	});
 });
+
+// (Optional) Active an item if it has the class "is-active"
+$('.dl_accordion > .dl_accordion_item.is-active')
+	.children('.dl_accordion_panel')
+	.slideDown();
+
+$('.dl_accordion > .dl_accordion_item').on('click', function () {
+	// Cancel the siblings
+	$(this)
+		.siblings('.dl_accordion_item')
+		.removeClass('is-active')
+		.children('.dl_accordion_panel')
+		.slideUp();
+	// Toggle the item
+	$(this)
+		.toggleClass('is-active')
+		.children('.dl_accordion_panel')
+		.slideToggle('ease-out');
+});
