8e9a44f36b856d253d79c97bd576bea73e9c9c93
Lingkon <lingkon909@gmail.com>
2023-04-20 17:05:30 +0600

    editing

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-$(function () {
-	$(function () {
-		var icons = {
-			header: 'ui-icon-plus',
-			activeHeader: 'ui-icon-closethick',
-		};
-		$('#accordion').accordion({
-			icons: icons,
-		});
-		$('#toggle').on('click', function () {
-			if ($('#accordion').accordion('option', 'icons')) {
-				$('#accordion').accordion('option', 'icons', null);
-			} else {
-				$('#accordion').accordion('option', 'icons', icons);
-			}
-		});
-	});
-});
 $(document).ready(function () {
 	$('.icons-slider').slick({
 		slidesToShow: 5,
